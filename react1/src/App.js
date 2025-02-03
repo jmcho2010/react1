b89ad6eb9f0bc3b81d7149170e2cf664cb55327b
jmcho2010 <projm2786@gmail.com>
2025-02-03 11:25:41 +0900

    ㅅㄷㄴㅅ

---
+++ react1/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+        <h1>이번엔 확실하게 실행되나?</h1>
+    </div>
+  );
+}
+
+export default App;
