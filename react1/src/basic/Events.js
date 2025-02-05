8455d19cdbd07d02bafa927ad0f5f1a23cee5d4a
jmcho2010 <projm2786@gmail.com>
2025-02-05 14:50:25 +0900

    ㅅㄷㄴㅅ

---
+++ react1/src/basic/Events.js
@@ -0,0 +1,15 @@
+// Events
+// JS에서 조금 개념을 추가했다 보면됨
+// 특징 : 일반함수로 선언하기보다는 arrow function을 적극적으로 활용
+
+import React from 'react';
+function Shoot(){
+
+    return(
+        <button onClick={}>힘껏 치세요!</button>
+    );
+
+}
+
+
+export default App;
--- react1/src/basic/quiz.js
+++ react1/src/basic/quiz.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
 // 아래의 코드에서 개선사항이 있는지 확인해볼것.
+// 개선사항 : 타입을 잘 맞춰주세요. 문자는 문자. 숫자는 숫자로.
+//  -> 숫자형 데이터는 중괄호를 사용하여 JS 표현식으로 전달하는것을
+//     권장함
 function Profile(props) {
     return (
       <div>
@@ -13,16 +16,17 @@ function Profile(props) {
   function App() {
     return (
       <div>
-        <Profile name="철수" age="열살" />
+        <Profile name="철수" age={10} />
       </div>
     );
   }
 
 //퀴즈2 : 이 코드는 동작하지 않는데 그 이유를 설명하고 변경하세요.
+// JSX 문법위반
   function Greeting({ isLoggedIn }) {
     return (
       <div>
-        isLoggedIn ? "환영합니다!" : "로그인해주세요"
+        {isLoggedIn ? "환영합니다!" : "로그인해주세요"}
       </div>
     );
   }
