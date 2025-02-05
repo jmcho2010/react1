bc536c1b226e7df03e1cceb0607a4d31351428e1
jmcho2010 <projm2786@gmail.com>
2025-02-05 17:52:10 +0900

    test

--- react1/src/basic/usingCss.js
+++ react1/src/basic/usingCss.js
@@ -1,6 +1,8 @@
 //usingCss.js
 import React from "react";
-
+//import styles from "./test.module.css";
+// sass/scss 인식을 못하면 npm i sass
+import './test.scss'
 function App(){
 
     const freeStyle = {
@@ -13,7 +15,7 @@ function App(){
     return(
         <>
             {/* <h1 style={freeStyle}>떠...떳냐?</h1> */}
-            <h1>모듈css를 붙일떄는 클래스네임을 작성시 규칙이 있음</h1>
+            {/* <h1 className={styles.test}>모듈css를 붙일떄는 클래스네임을 작성시 규칙이 있음</h1> */}
 
             <h1>비교용</h1>
         </>
