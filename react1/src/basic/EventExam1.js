9ba8d8520ba8ed580ab242559c1ef79ae4adc8ad
jmcho2010 <projm2786@gmail.com>
2025-02-05 16:14:15 +0900

    test

--- react1/src/basic/EventExam1.js
+++ react1/src/basic/EventExam1.js
@@ -32,9 +32,26 @@ function Quiz(){
 // 1. 특문(☆) 이모티콘과 숫자를 보여주는 컴포넌트를 만드세요
 // 2. 이모티콘을 클릭할 때마다 숫자가 1씩 증가해야 해요
 // 3. 숫자가 10이 되면 "최대 클릭 수에 도달했습니다!"라는 메시지를 표시하세요
+
 function Quiz2(){
-    const [count, setCount] = useState(0);
+    // 배열의 비구조화 할당 문법
+    const [count, setCount] = useState(1);
+
+    const clickCnt = () =>{
+        if (count <10){
+            setCount(count + 1);
+        }
+    }
+
+    return(
+        <div>
+            <div onClick={clickCnt}>
+               ♥ {count}
+            </div>
+            {count === 10 && <p>최대 클릭수에 도달함</p>}
+        </div>
+    );
 }
 
 
-export default Quiz;
+export default Quiz2;
---
+++ react1/src/basic/conditionalRender.js
@@ -0,0 +1,2 @@
+//conditionalRender.js
+
