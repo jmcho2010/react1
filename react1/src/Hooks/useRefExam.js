e6b350d6da585528cb33fe6bdb73ebfd03284ec2
jmcho2010 <projm2786@gmail.com>
2025-02-06 16:34:36 +0900

    ㅅㄷㄴㅅ

--- react1/src/Hooks/useRefExam.js
+++ react1/src/Hooks/useRefExam.js
@@ -25,33 +25,55 @@ import React, { useState, useRef, useEffect } from "react";
 //   }
 
 //current : 현재값 
-function MagicBox(){
-    const boxRef = useRef();
+// function MagicBox(){
+//   // inputRef는 마법 지팡이예요!
+//   const inputRef = useRef();
 
-    const changeColor = () =>{
-        //boxRef.current : 요소선택 / style.backgroundColor : DOM traversing
-        boxRef.current.style.backgroundColor =
-        '#' + Math.floor(Math.random()*16777215).toString(16);
-    };
+//   const focusInput = () => {
+//     // 마법 지팡이로 입력창을 가리키면 반짝!
+//     // 전역 event.target과도 유사함.
+//     inputRef.current.focus();
+//   };
 
-    return(
-        <div>
-            <div
-                ref={boxRef}
-                style={{
-                    width: '250px',
-                    height: '250px',
-                    backgroundColor: 'hotpink',
-                    margin: '25px'
-                }}
-            >
+//   return (
+//     <div>
+//     <h1>퀴즈 버튼을 누를시 focusing을 통해
+//         inputbox를 반짝반짝 하는 효과를 줘볼것
+//         포커싱은 focus 메서드를 통해 처리가능
+//         (vanilaJS에서도 가능)
+//     </h1>
+//       <input 
+//         ref={inputRef} 
+//         type="text" 
+//         placeholder="여기에 글자를 써보세요"
+//       />
+//       <button onClick={focusInput}>
+//         입력창 반짝이게 하기
+//       </button>
+//     </div>
+//   );
+// }
 
-            </div>
-            <button onClick={changeColor}>
-                상자색깔 변경하기
-            </button>
-        </div>
-    )
-}
+function App() {
+    const [inputValue, setInputValue] = useState("");
+    const previousInputValue = useRef("");
+  
+    useEffect(() => {
+      previousInputValue.current = inputValue;
+    }, [inputValue]);
+  
+    return (
+      <>
+        <input
+          type="text"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <h2>Current Value: {inputValue}</h2>
+        <h2>Previous Value: {previousInputValue.current}</h2>
+      </>
+    );
+  }
+  
 
-export default MagicBox;
+export default App;
