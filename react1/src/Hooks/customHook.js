3024faa345dc3e72b6fb00c65245577fe538bee8
jmcho2010 <projm2786@gmail.com>
2025-02-07 13:44:50 +0900

    test

---
+++ react1/src/Hooks/customHook.js
@@ -0,0 +1,57 @@
+// customHook.js
+
+// 커스텀훅 : 상태로직을 재사용할수 있도록 하는 기능.
+// 함수이름을 정의해놓고 useState, useEffect등을 사용하여 구현.
+
+// 커스텀훅은 컴포넌트 재사용성의 궁극의 정수.
+//  -> 기존 컴포넌트 구성코드의 중복도 줄일수 있다는 장점이 있음.
+//  -> API 호출, 폼데이터 관리, 중복되는 애니메이션 기능들을 커스텀훅으로 
+//     만들어서 사용.
+
+// 리엑트 프로젝트의 일반적 폴더구조.
+// src
+//  -> component (ui 컴포넌트 저장 폴더)
+//  -> hooks (커스텀 훅 저장 폴더) 
+//  -> utils (유틸리티 함수 저장폴더)
+//  -> pages (페이지 컴포넌트 저장폴더)
+//  -> styles (css, scss)
+//  -> assets  (이미지, 폰트등의 정적 파일)
+//  -> context (리액트 컨텍스트 저장 폴더.)
+// app.js
+
+// 폴더구조에서 커스텀훅과 유틸을 나눈 이유.
+// 커스텀훅은 react훅을 활용하여 공통적 코드를 처리
+//  -> 컴포넌트의 상태, 라이프사이클 영향을 많이 받음.
+// 유틸은 react와 무관한 코드들, 어디서나 호출되면서 컴포넌트에 종속x
+//  -> 단순한 js
+
+// 커스텀훅 특징
+// 재사용성, 캡슐화(코드 보안/ 유지보수성), 모듈성
+
+// 커스텀훅의 규칙
+
+import React, {useState, useEffect} from "react";
+
+function LoadData(){
+
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+      fetch("https://jsonplaceholder.typicode.com/todos")
+        .then((res) => res.json())
+        .then((data) => setData(data));
+    }, []);
+  
+    return (
+      <>
+        {data &&
+          data.map((item) => {
+            return <p key={item.id}>{item.title}</p>;
+          })}
+      </>
+    );
+
+}
+
+
+export default LoadData;
--- react1/src/index.js
+++ react1/src/index.js
@@ -7,7 +7,7 @@ import Hooks4 from './Hooks/useRefExam.js'
 import Hooks5 from './Hooks/useReducer.js'
 import Hooks6 from './Hooks/useReducer2.js'
 import Hooks7 from './Hooks/useMemo.js'
-import Test from './basic/test.js'
+import Hooks8 from './Hooks/customHook.js'
 //Hooks 전용 index
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Test />);
