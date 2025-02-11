75d940afae2c24d956578df27ffca7152c0b7411
jmcho2010 <projm2786@gmail.com>
2025-02-11 17:35:35 +0900

    test

--- react1/src/DataChange/TodoList.js
+++ react1/src/DataChange/TodoList.js
@@ -1,12 +1,35 @@
 import { useState } from "react";
-
+import { useSelector, useDispatch } from "react-redux";
+import {addTodo, removeTodo, toggleTodo} from './todoSlice';
 
 function TodoList(){
     const [newTodo, setNewTodo] = useState('');
+    const dispatch = useDispatch();
+    const {items, filter} = useSelector(state => state.todos);
+
+    // 할일 필터링.
+    const filteredTodos = items.filter(todo => {
+        if(filter === 'completed'){
+            return todo.completed;
+        }
+        if(filter === 'active'){
+            return !todo.completed;
+        }
+        return true;
+    })
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        if(newTodo.trim()){
+            dispatch(addTodo(newTodo));
+            setNewTodo('');
+        } // trim : 양끝 공백 제거.
+    };
+
 
     return(
         <div>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <input 
                     value={newTodo}
                     onChange={(e) => setNewTodo(e.target.value)}
@@ -22,10 +45,36 @@ function TodoList(){
             </div>
 
         {/*화면 랜더링*/}
-            <ul>
-                {filteredTodos.map}
-            </ul>
+        <ul>
+        {filteredTodos.map(todo => (
+          <li key={todo.id}>
+            <input
+              type="checkbox"
+              checked={todo.completed}
+              onChange={() => dispatch(toggleTodo(todo.id))}
+            />
+            <span style={{ 
+              textDecoration: todo.completed ? 'line-through' : 'none' 
+            }}>
+              {todo.text}
+            </span>
+            <button onClick={() => dispatch(removeTodo(todo.id))}>
+              삭제
+            </button>
+          </li>
+        ))}
+      </ul>
         </div>
     )
 
 }
+
+export default TodoList;
+
+// 퀴즈
+// TodoList컴포넌트의 ul태그영역을 참고하여 다음의 기능들을 완성
+// removeTodo, toggleTodo, setFilter
+
+// 이 리덕스 구조 기반의 어플리케이션을 실행하기위한
+// store 파일 정의와 app을 정의하여 실행까지 진행해주세요.
+
